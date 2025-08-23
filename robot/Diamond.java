9d3d266235e21a7166ad4c3bb15ea89d5489663f
Aashritha12345 <as8016@pleasantonusd.net>
2025-08-23 04:26:56 +0000

    new diamond creation

--- robot/Diamond.java
+++ robot/Diamond.java
@@ -14,264 +14,163 @@ public class Diamond implements Directions{
         // The line below creates a Robot that we will refer to as rob
         // Find out what the numbers and direction do!
         // Put rob in a better location for your initials.
-        Robot rob = new Robot(15,2,South,90);
+        Robot rob = new Robot(20,11,South,90);
         World.setDelay(1);
         // Want a second robot?  No prob.  They are cheap :)
         //Robot dude = new Robot(7,5,West,9);
         // examples of commands you can invoke on a Robot
-        rob.move();// move one step in the direction it is facing
+       { 
+        for(int num=1; num <= 8; num++) 
+        {
+       rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        }
+      for (int num=1; num <= 7; num++) 
+      {
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+      }
 
+      {rob.turnLeft();}
+       for (int num= 1; num <= 7; num++) 
+       { 
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.putBeeper();
+        
+       }
+      {rob.turnLeft();
+    }
 
-        // starting the letter R
-        rob.move();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
+    for(int num= 1; num <= 6; num++)
+    {
+      rob.turnLeft();
         rob.move();
         rob.turnLeft();
         rob.turnLeft();
         rob.turnLeft();
         rob.move();
         rob.putBeeper();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
-        rob.move();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.turnLeft();
-        rob.move();
-        rob.putBeeper();
+
+    }
+        
+    {
+      rob.turnLeft();
+      rob.turnLeft();
+    }
+
+      for(int num= 1; num <= 3; num++) 
+      {
         rob.move();
 
+      }
+       {
+         rob.turnLeft();
+         rob.move();
+         rob.turnLeft();
+         rob.turnLeft();
 
+         for(int num= 1; num<=4; num++) 
+         { 
+            rob.putBeeper();
+            rob.move();
 
-       
+         }
+          
+         {
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        
 
+    }
+       for(int num= 1; num <= 2; num++)
+        {
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
     
 
+       }
+
+       {rob.turnLeft();
+        rob.turnLeft();
+    
+    }
+       
+       }
+
+      for(int num= 1; num <= 4; num++ ){
+
+        rob.move();
+        rob.putBeeper();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();        
+      }
+
+      {
+        rob.turnLeft();
+        rob.move();
+        rob.turnLeft();
+        rob.turnLeft();
+        rob.turnLeft();
+
+      }
+
+      for(int num= 1; num <= 4; num++) 
+      {
+         rob.turnLeft();
+         rob.move();
+         rob.turnLeft();
+         rob.turnLeft();
+         rob.turnLeft();
+         rob.move();
+         rob.putBeeper();
+
+      }
 
 
-        
+      {
+         rob.turnLeft();
+         rob.turnLeft();
+         rob.move();
+         rob.turnLeft();
+         rob.turnLeft();
+         rob.turnLeft();
+         rob.move();
+         rob.putBeeper();
+         rob.move();
+         rob.move();
+         rob.move();
+
+      }
+
+    }
+
         
 
 
