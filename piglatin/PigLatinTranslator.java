10fedf007a4f83bf135549c6ec4f751c89b5af00
Aashritha12345 <as8016@pleasantonusd.net>
2025-10-21 22:00:59 +0000

    i attempted to write a code that checks if the vwel is in front of the word or not

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -15,33 +15,56 @@ public class PigLatinTranslator {
     public static String translate(String input) {
         System.out.println("  -> translate('" + input + "')");
 
-        String result = "aeiouy";
-        String test=  "a";
-        if (vowels.indexOf(test)!=-1)
-        {
-
-               System.out.println(test + "is a vowel");
-        }
         
-        // TODO: translate a string input, store in result.
-        // The input to this function could be any English string.
-        // It may be made up of many words.
-        // This method must call translateWord once for each word in the string.
-        result = translateWord(input);
+        String result = translateWord(input);
 
         return result; 
     }
 
     private static String translateWord(String input) {
         System.out.println("  -> translateWord('" + input + "')");
+String result = "";
+         if (input.startsWith("a")|| input.startsWith("e")||input.startsWith("i")||input.startsWith("o")||input.startsWith("u")||input.startsWith("y"))   {
 
-        String result = "";
-         
-        // TODO: Replace this code to correctly translate a single word.
+
+           return input + "ay";
+
+         }      
+        
+        if (!(input.charAt(1).equals('a'))|| !(input.charAt(1).equals('e'))||!(input.charAt(1).equals('i'))||(input.charAt(1).equals('o'))||(input.charAt(1).equals('u'))||(input.charAt(1).equals('y'))){
+        
+            return input.substring(2) + input.substring(0,2) + 'ay';
+
+
+        } else {
+
+            return input.substring(1)+ input.substring (0,1) + 'ay';
+        }
+        
+        
+        
+         // TODO: Replace this code to correctly translate a single word.
         // Start here first!
+       // String vowels = "aeiouy";
+       // if (vowels.indexOf(test)!=-1)
+        //for (int i=0;i<input.length(); i++)
+      //  {
+       //        System.out.println(test + "is a vowel");
+       // }
+        //char a;
+        //a.isuUppercase()
+        //Pig 
+        //g Pi ay
+        //G pi ay
+        //if (input.charAt(0).isuUppercase()){
+            //the input have first char capped 
+        //}
+        // TODO: translate a string input, store in result.
+        // The input to this function could be any English string.
+        // It may be made up of many words.
+        // This method must call translateWord once for each word in the string.
         
         // This is the first place to work.
-        result = input; // delete this line
 
         return result;
     }
