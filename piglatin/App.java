6cbb8a4519253d7e155539f9739cd664e1d49fb9
Aashritha12345 <as8016@pleasantonusd.net>
2025-10-27 21:39:56 +0000

    Project working, turning in.

--- piglatin/App.java
+++ piglatin/App.java
@@ -36,7 +36,9 @@ public class App {
             input.readFromString("TestBook", "Dog\nCat\nMouse");
 
             // Example reading from a URL
-            //input.readFromUrl("Romeo and Juliette", "https://gutenberg.pglaf.org/cache/epub/1513/pg1513.txt");
+            input.readFromUrl("Romeo and Juliette", "https://gutenberg.pglaf.org/cache/epub/1513/pg1513.txt");
+                       input.readFromUrl("Catherine the Great", "https://www.gutenberg.org/cache/epub/77133/pg77133.txt");
+
 
             input.printlines(0,2);
             Book output = PigLatinTranslator.translate(input);
