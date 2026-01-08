22e1d9cb006c22dc80ddbf9aaf1dd72719a782ca
Aashritha12345 <as8016@pleasantonusd.net>
2026-01-08 23:05:23 +0000

    I am done with the warmup

--- warmup/Main.java
+++ warmup/Main.java
@@ -3,7 +3,18 @@ package warmup;
 public class Main {
 
     public static void main(String[] args) {
-        // Your code here
+        int a=0;
+        int sum=0;
+        while(a<1000)
+        {
+        if(a%3==0|| a%5==0)
+        {
+          sum+=a;
+       }
+          a++;
+        }
+                    
+        System.out.println("The sum of all the multiples of 3 or 5 below 1000 is: " + sum);
 
     }
 }
