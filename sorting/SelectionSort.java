ab3b86f844779748637f1b6cd99201e942df4017
Aashritha12345 <as8016@pleasantonusd.net>
2026-02-01 20:48:41 +0000

    I have worked on the code for selection sort and I think that I have finished  it

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,5 +4,24 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+
+
+        for (int i=0; i<input.length-1; i++){
+            //it ilterates through the whole array and use the smallest element to swap with the current index so that every ilteration the smallest element is placed at the front of the unsorted part of the array
+            int minIndex = i;
+            //min index is the index of the smallest element
+            for (int j=i+1; j<input.length; j++){
+                //so j is the index of the element we are comparing to the smallest element
+                if (input[j] < input[minIndex]){
+                    //if the element at j is smaller than the element at minIndex
+                    minIndex = j;
+                    //then we update minIndex to be j
+                }
+            }
+            //after we finish the inner loop, we swap the smallest element with the element at index i
+            int temp = input[i];
+            input[i] = input[minIndex];
+            input[minIndex] = temp;
+        }
     }
 }
